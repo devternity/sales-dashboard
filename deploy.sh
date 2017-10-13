8892e3ff026b2e50abddf6c2fef06cb35ecfc54c
Andrey Adamovich <andrey@aestasit.com>
2017-10-13 10:25:42 +0300

    moved artifact preparation to deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -7,12 +7,18 @@ SSH="ssh -o StrictHostKeyChecking=no -i $DEPLOY_KEY $DEPLOY_USER@$DEPLOY_HOST"
 openssl aes-256-cbc -K $encrypted_1645300b04d0_key -iv $encrypted_1645300b04d0_iv -in deploy.key.enc -out $DEPLOY_KEY -d
 chmod 400 $DEPLOY_KEY
 
+# Decrypt configuration
+# TODO: so far it is a manual copy
+# openssl enc -aes-256-cbc -pass env:SECRET_PASSWORD -d -a -in config/devternity.yml -out config/devternity.yml
+# openssl enc -aes-256-cbc -pass env:SECRET_PASSWORD -d -a -in config/devternity.yml -out config/devternity.yml
+# openssl enc -aes-256-cbc -pass env:SECRET_PASSWORD -d -a -in config/devternity.yml -out config/devternity.yml
+
+# Create artifact 
+rm -rf dashboard.tgz
+tar -czf dashboard.tgz ./assets ./dashboards ./jobs ./public ./widgets ./config.ru ./Gemfile* ./dashing.service
+
 # Copy artifact to remote host
 scp -o StrictHostKeyChecking=no -i $DEPLOY_KEY dashboard.tgz $DEPLOY_USER@$DEPLOY_HOST:/tmp
-
-# Copy configuration
-# TODO: so far it is a manual copy
-# openssl enc -aes-256-cbc -pass env:DEPLOY_PASSWORD -d -a -in "config/devternity.yml" 2> /dev/null
 
 # Deploy dashboard code
 $SSH sudo mkdir -p /dashboard/config
