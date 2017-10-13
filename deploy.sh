6f2513963ebc6ed0197ccbf958b2e7b33cbb4164
Andrey Adamovich <andrey@aestasit.com>
2017-10-13 09:42:40 +0300

    moved decryption to deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,20 +1,22 @@
 #!/usr/bin/env bash
 
+# SSH parameters
 SSH="ssh -o StrictHostKeyChecking=no -i $DEPLOY_KEY $DEPLOY_USER@$DEPLOY_HOST"
 
-# Give correct permissions to the key
+# Decrypt deployment key
+openssl aes-256-cbc -K $encrypted_1645300b04d0_key -iv $encrypted_1645300b04d0_iv -in deploy.key.enc -out $DEPLOY_KEY -d
 chmod 400 $DEPLOY_KEY
 
 # Copy artifact to remote host
 scp -o StrictHostKeyChecking=no -i $DEPLOY_KEY dashboard.tgz $DEPLOY_USER@$DEPLOY_HOST:/tmp
 
-# Deploy dashboard code
-$SSH sudo mkdir -p /dashboard/config
-$SSH sudo tar -zxvf /tmp/dashboard.tgz --no-same-owner -C /dashboard
-
 # Copy configuration
 # TODO: so far it is a manual copy
 # openssl enc -aes-256-cbc -pass env:DEPLOY_PASSWORD -d -a -in "config/devternity.yml" 2> /dev/null
+
+# Deploy dashboard code
+$SSH sudo mkdir -p /dashboard/config
+$SSH sudo tar -zxvf /tmp/dashboard.tgz --no-same-owner -C /dashboard
 
 # Restart service
 $SSH <<EOF
