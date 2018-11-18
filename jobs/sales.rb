f1866eb108cceee38fa1fbb18006450309eb681b
Andrey Adamovich <andrey@aestasit.com>
2018-11-18 12:17:25 +0200

    unified with event dashboard

--- jobs/sales.rb
+++ jobs/sales.rb
@@ -10,10 +10,11 @@ require 'firebase'
 # Load configuration parameters.
 ###########################################################################
 
-$global_config = YAML.load_file('./config/devternity.yml')
-$firebase_config = JSON.parse(open($global_config['firebase_config']) {|f| f.read})
+$firebase_json = File.open('./config/firebase-sales.json') { |file| file.read }
+$firebase_config = JSON.parse($firebase_json)
+$base_url = "https://#{$firebase_config['project_id']}.firebaseio.com/"
 
-DT2018_PRODUCTS = ['DT_RIX_18']
+DT2018_PRODUCTS = [ 'DT_RIX_18' ]
 DT2018_DAY1_KEYNOTE = 'Main Day Pass'
 DT2018_ERROR_TICKETS_EVENT = 'Error: Tickets data error'
 
@@ -37,51 +38,35 @@ class DevternityFirebaseStats
 
   attr_reader :client
 
-  def initialize(opts)
+  def initialize(opts, auth_token)
     base_url = "https://#{opts['project_id']}.firebaseio.com/"
-    auth_token = opts['auth_token']
     @client = Firebase::Client.new(base_url, auth_token)
   end
 
   def call(job)
-    begin
-      capacities = WORKSHOP_CAPACITIES.merge({ "Main Hall" => MAIN_DAY_CAPACITY })
-      sales = counts()
-      event_stats = sales[:tickets]
-        .sort_by {|name, count| -count}
-        .map {|name, count| { label: name + remaining(capacities, count), value: count }}
-      send_event('tickets', { title: "#{sales[:total]} tickets purchased", moreinfo: "Total #{sales[:total]}", items: event_stats })
-      day1Tickets = sales[:tickets][DT2018_DAY1_KEYNOTE]
-      send_event('keynotes', { max: MAIN_DAY_CAPACITY, moreinfo: "#{day1Tickets}/#{MAIN_DAY_CAPACITY}", value: day1Tickets })
-      send_event('workshops', { max: WORKSHOP_CAPACITY, moreinfo: "#{sales[:total] - day1Tickets}/#{WORKSHOP_CAPACITY}", value: sales[:total] - day1Tickets })
-    end
-  rescue => e
-    puts e.backtrace
-    puts "\e[33mFor the Firebase credentials check ./config/firebase-legacy.json.\n\tError: #{e.message}\e[0m"
+
+    capacities = WORKSHOP_CAPACITIES.merge({ "Main Hall" => MAIN_DAY_CAPACITY })
+
+    sales = counts()
+
+    event_stats = sales[:tickets]
+      .sort_by { |name, count| -count }
+      .map { |name, count| { label: name + remaining(capacities, count), value: count } }
+
+    day1Tickets = sales[:tickets][DT2018_DAY1_KEYNOTE]
+
+    send_event('tickets', { title: "#{sales[:total]} tickets purchased", moreinfo: "Total #{sales[:total]}", items: event_stats })
+    send_event('keynotes', { max: MAIN_DAY_CAPACITY, moreinfo: "#{day1Tickets}/#{MAIN_DAY_CAPACITY}", value: day1Tickets })
+    send_event('workshops', { max: WORKSHOP_CAPACITY, moreinfo: "#{sales[:total] - day1Tickets}/#{WORKSHOP_CAPACITY}", value: sales[:total] - day1Tickets })
+
   end
 
   private
 
   def remaining(capacities, count)
-    max = capacities.max_by{|k,v| v}
+    max = capacities.max_by{ |k,v| v }
     capacities.delete(max.first)
     " (#{max.last - count} out of #{max.last} left in #{max.first})"
-  end
-
-  def raw_applications
-    response = @client.get('applications')
-    raise Error.new "DT error #{response.code}" unless response.success?
-    response.body
-  end
-
-  def clean_applications(data = raw_applications())
-    dt2018_data = data.select { |id, application| DT2018_PRODUCTS.include?(application['product']) }
-    dt2018_data.map { |id, application|
-      tickets = application['tickets'] || [DT2018_ERROR_TICKETS_EVENT]
-      tickets = [tickets] unless tickets.is_a? Array
-      tickets = tickets.map { |ticket| ticket['event'] || DT2018_ERROR_TICKETS_EVENT }
-      [ id, { tickets: tickets } ]
-    }.to_h
   end
 
   def counts(data = clean_applications())
@@ -111,12 +96,34 @@ class DevternityFirebaseStats
 
   end
 
+  def clean_applications(data = raw_applications())
+    if !data.nil? && !data.empty?
+      dt2018_data = data.select { |id, application| DT2018_PRODUCTS.include?(application['product']) }
+      return dt2018_data.map { |id, application|
+        tickets = application['tickets'] || [DT2018_ERROR_TICKETS_EVENT]
+        tickets = [ tickets ] unless tickets.is_a? Array
+        tickets = tickets.map { |ticket| ticket['event'] || DT2018_ERROR_TICKETS_EVENT }
+        [ id, { tickets: tickets } ]
+      }.to_h
+    else 
+      return { }
+    end
+  end
+
+  def raw_applications
+    response = @client.get('applications')
+    raise "DT error #{response.code} (#{response.body})" unless response.success?
+    response.body
+  end
+
 end
 
 ###########################################################################
 # Job's schedules.
 ###########################################################################
 
+stats = DevternityFirebaseStats.new($firebase_config, $firebase_json)
+
 SCHEDULER.every '15m', :first_in => 0 do |job| 
-  DevternityFirebaseStats.new($firebase_config).call(job)
+  stats.call(job)
 end
