2fa5e42b655d67deb5a1207cd2115ed74743c0ff
Andrey Adamovich <andrey@aestasit.com>
2018-11-18 14:36:05 +0200

    cleanup before deployment

--- deploy.sh
+++ deploy.sh
@@ -29,6 +29,7 @@ rm -rf dashboard.tgz
 rm -rf config/*
 
 # Deploy dashboard code
+$SSH sudo rm -rf /dashboard/*
 $SSH sudo mkdir -p /dashboard/config
 $SSH sudo tar -zxvf /tmp/dashboard.tgz --no-same-owner -C /dashboard
 
